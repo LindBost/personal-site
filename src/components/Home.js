9a71668256a4f5cfb2366d3e7bd8f24e093606fd
Linda Blom <linda.bostrom@tele2.com>
2021-02-02 15:06:08 +0100

    card component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React, { useState }  from 'react'
 import MeContainer from './MeContainer'
 import SayHelloContainer from './SayHelloContainer'
+import Card from './Card';
 
 const Home = () => {
     return (
@@ -8,9 +9,16 @@ const Home = () => {
             HOME Här ritar jag ut MeContainer och SayHelloContainer
             <MeContainer />
             <SayHelloContainer />
+{ /*           <Card 
+            title='card title'
+            imageUrl='https://www.elegantthemes.com/blog/wp-content/uploads/2020/02/000-Online-Code-Editors.png'
+            body='body body body body body body body body body body body'
+/> */}
         </div>
     )
 }
 
 
 export default Home;
+
+//https://www.youtube.com/watch?v=Q3I_NwaCZI8 - create a card with title 
--- src/components/MeContainer.js
+++ src/components/MeContainer.js
@@ -1,5 +1,6 @@
 import React, { useState, useCallback }  from 'react'
 import {useHistory} from 'react-router-dom';
+import Card from './Card';
 
 const MeContainer = () => {
 
@@ -11,6 +12,11 @@ const MeContainer = () => {
             MECONTAINER Här kommer det va en grid (så om man klickar på blog så kommer man dit )
             
             <div onClick={displayBlog}>klicka för att se blog</div>
+            <Card 
+            title='card title'
+            imageUrl='https://www.elegantthemes.com/blog/wp-content/uploads/2020/02/000-Online-Code-Editors.png'
+            body='body body body body body body body body body body body'
+            />
         </div>
     )
 }
