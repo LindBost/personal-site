36dfb7d0672a0a2f533bc6b0ae25c67f0a5de0ab
Linda Blom <linda.bostrom@tele2.com>
2021-02-02 14:13:03 +0100

    hero styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,26 @@
 import React from 'react'
+import './Header.css';
+import hero from './assets/hero.mp4';
 
 const Header = () => {
     return (
-        <div>
-            HEADER
+        <div className='header'>
+            <div className='heroBg'>
+                <video className='videoBg' autoPlay loop muted>
+                    <source src={hero} type='video/mp4'/>
+                </video>
+            </div>
+            <div className='heroContent'>
+                <div className='heroItems'>
+                    <h1 className='heroH1'>test1</h1>
+                    <p className='heroP'>test2</p>
+                </div>
+            </div>
         </div>
     )
 }
 
 
 export default Header;
+
+//https://github.com/LindBost/my-gatsby-site/blob/master/react-gatsby-travel-v1/src/components/Hero.js
