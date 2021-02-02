345ade2b9d7527ed75b5ad491269b2ac63e90962
Linda Blom <linda.bostrom@tele2.com>
2021-02-02 16:33:56 +0100

    me h2

--- src/components/MeContainer.js
+++ src/components/MeContainer.js
@@ -1,32 +1,35 @@
-import React  from 'react'
+import React from 'react'
 import Card from './Card';
 import './MeContainer.css';
 
 const MeContainer = () => {
 
     return (
-        <div className='gridContainer'>
-            <div className='card-items'>
-            <Card
-            title='Blog'
-            imageUrl='https://www.elegantthemes.com/blog/wp-content/uploads/2020/02/000-Online-Code-Editors.png'
-            body='body body body body body body body body body body body'
-            path='/blog'
-            />
+        <div>
+            <h2>Me.</h2>
+            <div className='gridContainer'>
+                <div className='card-items'>
+                    <Card
+                        title='Blog'
+                        imageUrl='https://www.elegantthemes.com/blog/wp-content/uploads/2020/02/000-Online-Code-Editors.png'
+                        body='body body body body body body body body body body body'
+                        path='/blog'
+                    />
 
-            <Card 
-            title='github'
-            imageUrl='https://www.elegantthemes.com/blog/wp-content/uploads/2018/04/Best-Code-and-Text-Editors.png'
-            body='body body body body body body body body body body body'
-            path='/github'
-            />
+                    <Card
+                        title='github'
+                        imageUrl='https://www.elegantthemes.com/blog/wp-content/uploads/2018/04/Best-Code-and-Text-Editors.png'
+                        body='body body body body body body body body body body body'
+                        path='/github'
+                    />
 
-            <Card 
-            title='Me'
-            imageUrl='https://kinsta.com/wp-content/uploads/2020/04/code-review-tools.png'
-            body='body body body body body body body body body body body'
-            path='/blog'
-            />
+                    <Card
+                        title='Me'
+                        imageUrl='https://kinsta.com/wp-content/uploads/2020/04/code-review-tools.png'
+                        body='body body body body body body body body body body body'
+                        path='/blog'
+                    />
+                </div>
             </div>
         </div>
     )
