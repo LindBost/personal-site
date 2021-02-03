3d7b8f2bf74314498ce3e796f7b49347eb10f0b4
Linda Blom <linda.bostrom@tele2.com>
2021-02-03 17:50:34 +0100

    update github

--- src/components/Github/Github.js
+++ src/components/Github/Github.js
@@ -1,26 +1,30 @@
-import React, { useEffect, useCallback }  from 'react'
-import {useHistory, Link} from 'react-router-dom';
+import React, { useEffect, useCallback } from 'react'
+import { useHistory, Link } from 'react-router-dom';
+import './Github.css';
+import githubimg from '../assets/githubimg.png';
 
 const Github = () => {
 
     const history = useHistory();
     const handleBack = useCallback(() => history.push('/'), [history]);
-    
+
     const toGithub = () => {
         window.open('https://github.com/LindBost', "_blank")
     }
 
 
     return (
-        <div>
-            Jag har klickat på Github  
-            <h2>Github</h2>
-            <div>
-                kolla
-                <button type="button" onClick={toGithub}>To Github</button>
+            <div className='github-container'>
+            <h2>Github.</h2>
+            <div className='github-logo'>
+                <img src={githubimg} alt='logo' />
             </div>
-            <div onClick={handleBack}>Tillbaka</div>
-        </div>
+            <p className='github-text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sodales enim id libero commodo euismod. Aenean sit amet felis iaculis, finibus urna vel, consectetur dolor. Duis id lacus non odio cursus convallis pretium ac sapien.</p>
+            <div className='knapp-group'>
+                <button className='knapp' type="button" onClick={toGithub}>To Github</button>
+                <div className='knapp'onClick={handleBack}>Tillbaka</div>
+            </div>
+            </div>
     )
 }
 
