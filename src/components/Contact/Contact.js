dfc49706e0fefe70c6385705eb132d1f5491ba5a
linda <linda-bostrom@hotmail.se>
2021-02-21 19:34:52 +0100

    contact styling

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -6,9 +6,14 @@ const Contact = () => {
     const history = useHistory();
     const handleBack = useCallback(() => history.push('/'), [history]);
 
+    const toLinkedIn = () => {
+        window.open('https://www.linkedin.com/in/linda-blom-05150696/', "_blank")
+    }
+
+
     return (
-        <div>
-            Jag har klickat på Contact  
+        <div> 
+            <button className='knapp' type="button" onClick={toLinkedIn}>To linkedin</button>
             <div onClick={handleBack}>Tillbaka</div>
         </div>
     )
--- src/components/SayHelloContainer.js
+++ src/components/SayHelloContainer.js
@@ -4,16 +4,25 @@ import Card from './Card';
 
 
 const SayHelloContainer = () => {
+
+    const toLinkedIn = () => {
+        window.open('https://www.linkedin.com/in/linda-blom-05150696/', "_blank")
+    }
+
+
     return (
         <div className='hello-container'>
             <h2>Say Hello.</h2>
+            <h1>linda-bostrom@hotmail.se</h1>
+            <p className='hello-text'>I love to be creative and build things. Anything from my own projects like this page to Ikea furniture.</p>
             <div className='hello-card'>
-            <Card
+            {/*<Card
                 title='Say hello!'
                 imageUrl='https://www.elegantthemes.com/blog/wp-content/uploads/2020/02/000-Online-Code-Editors.png'
                 body='body body body body body body body body body body body'
                 path='/contact'
-            />
+            />*/}
+            <button className='knapp' type="button" onClick={toLinkedIn}>To linkedin</button>
             </div>
         </div>
     )
