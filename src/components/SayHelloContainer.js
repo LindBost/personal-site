bacb1a27926c053fae9c59d054adb5ce10e05b57
linda <linda-bostrom@hotmail.se>
2021-02-28 15:05:03 +0100

    styling

--- src/components/SayHelloContainer.js
+++ src/components/SayHelloContainer.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import './SayHelloContainer.css';
-import Card from './Card';
 
 
 const SayHelloContainer = () => {
@@ -9,20 +8,18 @@ const SayHelloContainer = () => {
         window.open('https://www.linkedin.com/in/linda-blom-05150696/', "_blank")
     }
 
+    const toGithub = () => {
+        window.open('https://github.com/LindBost', "_blank")
+    }
 
     return (
         <div className='hello-container'>
             <h2>Say Hello.</h2>
-            <h1>linda-bostrom@hotmail.se</h1>
+            <h1 className='hello-email'>linda-bostrom@hotmail.se</h1>
             <p className='hello-text'>I love to be creative and build things. Anything from my own projects like this page to Ikea furniture.</p>
             <div className='hello-card'>
-            {/*<Card
-                title='Say hello!'
-                imageUrl='https://www.elegantthemes.com/blog/wp-content/uploads/2020/02/000-Online-Code-Editors.png'
-                body='body body body body body body body body body body body'
-                path='/contact'
-            />*/}
             <button className='knapp' type="button" onClick={toLinkedIn}>To linkedin</button>
+            <button className='knapp' type="button" onClick={toGithub}>To Github</button>
             </div>
         </div>
     )
