e0b189af1ad8a79fd825a4240e04c95a1f11ca42
Linda Blom <linda.bostrom@tele2.com>
2021-03-04 15:13:10 +0100

    add data to me page

--- src/components/MePage/MePage.js
+++ src/components/MePage/MePage.js
@@ -1,18 +1,47 @@
 import React, { useCallback } from 'react'
 import { useHistory } from 'react-router-dom';
+import data from '../data/data.json';
 
 const MePage = () => {
 
     const history = useHistory();
     const handleBack = useCallback(() => history.push('/'), [history]);
 
+
+    const fetchDatazz = data;
+
+    console.log('fetchDatazz', fetchDatazz)
+
+    const fetchData = () => {
+        const temp = [];
+
+        {data.forEach((item, index) => {
+            temp.push(
+            <div className='page-card' key={index}>
+                <img src={item.img} alt={item.alt} />
+                <div className='page-info'>
+                    <div className='text-wrap'>
+                        <div>
+                            {item.name}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+        })}
+
+    return temp;
+    }
+
+
     return (
-            <div>
+        <div>
             <h2>Me.</h2>
+            <div>{fetchData()}</div>
             <div className='knapp-group'>
-                <div className='knapp'onClick={handleBack}>Tillbaka</div>
+                <div className='knapp' onClick={handleBack}>Tillbaka</div>
             </div>
-            </div>
+        </div>
     )
 }
 
