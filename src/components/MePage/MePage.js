12e481596e104633f4353bbd2e42a80b744eb289
Linda Blom <linda.bostrom@tele2.com>
2021-03-08 22:29:55 +0100

    test

--- src/components/MePage/MePage.js
+++ src/components/MePage/MePage.js
@@ -1,6 +1,6 @@
-import React, { useCallback } from 'react'
+import React, { useState, useCallback } from 'react'
 import { useHistory } from 'react-router-dom';
-import {data} from '../data/data.js';
+import { data } from '../data/data.js';
 import './MePage.css';
 
 const MePage = () => {
@@ -8,25 +8,61 @@ const MePage = () => {
     const history = useHistory();
     const handleBack = useCallback(() => history.push('/'), [history]);
 
+    /*
+    const [isActive, setActive] = useState(true)
+
+
+    console.log('FÖÖRS', isActive)
+    const turnCard = () => {
+        
+        setActive(false);
+    }
+    */
+
     const fetchData = () => {
         const temp = [];
-
-        {data.forEach((item, index) => {
-            temp.push(
-            <div className='page-card' key={index}>
-                <img className='me-img' src={item.img} alt={item.alt} />
-                <div className='page-info'>
-                    <div className='text-wrap'>
-                        <div className='me-title'>
-                            {item.name}
+        {
+            data.forEach((item, index) => {
+                temp.push(
+                    <div className='page-card' key={index}>
+                        <img className='me-img' src={item.img} alt={item.alt} />
+                        <div className='page-info'>
+                            <div className='text-wrap'>
+                                <div className='me-title'>
+                                    {item.name}
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </div>
-        )
-        })}
+                )
+            })
+        }
 
-    return temp;
+        /*
+        {
+            data.forEach((item, index) => {
+                temp.push(
+                    <div id={index} className= {`page-card flip-card ${isActive ? "isFlipped" : ""}`} key={index} onClick={((e) => turnCard())}>
+                        <div className='flip-card-inner'>
+                            <div className='flip-card-front'>
+                                <img className='me-img' src={item.img} alt={item.alt} />
+                                <div className='page-info'>
+                                    <div className='text-wrap'>
+                                        <div className='me-title'>
+                                            {item.name}
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className='flip-card-back'>massa text</div>
+                        </div>
+                    </div>
+                )
+            })
+        }
+        */
+
+        return temp;
     }
 
 
